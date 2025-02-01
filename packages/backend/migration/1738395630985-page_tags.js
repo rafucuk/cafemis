1f2ab4941f3e9121f68627b1b324ae930978eb8d
rauf <raufsen11@gmail.com>
2025-02-01 10:56:09 +0300

    fix: aptal mistake

--- packages/backend/migration/1738395630985-page_tags.js
+++ packages/backend/migration/1738395630985-page_tags.js
@@ -2,12 +2,10 @@ export class PageTags1738395630985 {
     name = 'PageTags1738395630985'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "page" DROP COLUMN "tags"`);
         await queryRunner.query(`ALTER TABLE "page" ADD "tags" character varying(128) array NOT NULL DEFAULT '{}'`);
     }
 
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "page" DROP COLUMN "tags"`);
-        await queryRunner.query(`ALTER TABLE "page" ADD "tags" text array`);
     }
 };
